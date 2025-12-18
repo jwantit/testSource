24829e81ca118790d3c75eaa03edec4ecafc4f98
kimjiwon <kgw072089@gmail.com>
2025-12-18 20:12:38 +0900

    [Front] TestPage - feature 테스트3

--- frontend/src/main/java/com/test/test.java
+++ frontend/src/main/java/com/test/test.java
@@ -5,7 +5,7 @@ public class test {
     String test = "test";
 
     public String setTest(){
-        String update = "front~~~~~feature Test11";
+        String update = "front~~~~~feature Test1122";
 
         return update;
     }
